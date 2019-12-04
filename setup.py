dd961337d89a106b4a88555175312ad81f0fc0af
teslajoy <sanati@ohsu.edu>
2019-12-04 08:50:53 -0800

    fetch latest version

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
+import json
 
-__version__ = '0.0.2'
+__version__ = json.loads(open('reactome/reactome2py').read())['latestVersion']
 
 setup(
     name='reactome2py',
