1fff912d8c4d6b6670df986398b70b327ec8f68f
teslajoy <sanati@ohsu.edu>
2021-01-04 14:14:13 -0800

    python 3.8 tests and minor change to travis script

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: Apache Software License',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
